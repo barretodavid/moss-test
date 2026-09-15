5238620d41c59e7fad16a71998f6cd961b889621
David Barreto <barretollano@gmail.com>
2026-09-15 13:25:16 -0400

    Retire milestone 5 fixtures

--- tests/test_paging.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from m5app.paging import page_size
-
-class PagingTests(unittest.TestCase):
-    def test_in_range(self):
-        self.assertEqual(page_size(12), 12)
-    def test_upper_bound(self):
-        self.assertEqual(page_size(101), 100)
