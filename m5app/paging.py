9ab12729eb667ab12b2321233bd3c533b4b2b640
mossbot55 <324945211+mossbot55@users.noreply.github.com>
2026-09-16 02:49:55 +0000

    Add default_page_size() returning 12

--- m5app/paging.py
+++ m5app/paging.py
@@ -3,3 +3,6 @@ MAX_PAGE_SIZE = 100
 
 def page_size(value):
     return min(value, MAX_PAGE_SIZE)
+
+def default_page_size():
+    return 12
--- tests/test_paging.py
+++ tests/test_paging.py
@@ -1,8 +1,12 @@
 import unittest
-from m5app.paging import page_size
+from m5app.paging import page_size, default_page_size
 
 class PagingTests(unittest.TestCase):
     def test_in_range(self):
         self.assertEqual(page_size(12), 12)
     def test_upper_bound(self):
         self.assertEqual(page_size(101), 100)
+
+class DefaultPageSizeTests(unittest.TestCase):
+    def test_returns_twelve(self):
+        self.assertEqual(default_page_size(), 12)
